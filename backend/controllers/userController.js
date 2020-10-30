628d1ff4862c8ccb6b6edfdcc8cf23de233844d8
Travis <travis.lannoye@gmail.com>
2020-10-29 22:52:12 -0500

    Added registerUser route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,6 +20,35 @@ const authUser = asyncHandler(async (request, response) => {
 	}
 });
 
+const registerUser = asyncHandler(async (request, response) => {
+	const { name, email, password } = request.body;
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		response.status(400);
+		throw new Error('User already exists');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+
+	if (user) {
+		response.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		response.status(400);
+		throw new Error('Invalid user data');
+	}
+});
+
 const getUserProfile = asyncHandler(async (request, response) => {
 	const user = await User.findById(request.user._id);
 
@@ -36,4 +65,4 @@ const getUserProfile = asyncHandler(async (request, response) => {
 	}
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
