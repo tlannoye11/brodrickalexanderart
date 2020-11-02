842f2d37e834a9dff2763791ae1820638ff87075
Travis <travis.lannoye@gmail.com>
2020-11-02 10:53:32 -0600

    Added Update Profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -65,4 +65,30 @@ const getUserProfile = asyncHandler(async (request, response) => {
 	}
 });
 
-export { authUser, registerUser, getUserProfile };
+const updateUserProfile = asyncHandler(async (request, response) => {
+	const user = await User.findById(request.user._id);
+
+	if (user) {
+		user.name = request.body.name || user.name;
+		user.email = request.body.email || user.email;
+
+		if (request.body.password) {
+			user.password = request.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		response.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	} else {
+		response.status(404);
+		throw new Error('User not found');
+	}
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
