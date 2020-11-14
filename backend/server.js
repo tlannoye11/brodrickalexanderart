bbedf81b811b73864ea340260365045aa3e782f3
Travis <travis.lannoye@gmail.com>
2020-11-13 19:20:43 -0600

    Added back-end route and controller for orders

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -19,6 +21,7 @@ app.get('/', (request, response) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
