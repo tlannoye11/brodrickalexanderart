8743c4198f0db055b72a60bb357197fd2590da1c
Travis <travis.lannoye@gmail.com>
2020-11-17 15:52:00 -0600

    Added updateOrderToPaid route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,4 +48,26 @@ const getOrderById = asyncHandler(async (request, response) => {
 	}
 });
 
-export { addOrderItems, getOrderById };
+const updateOrderToPaid = asyncHandler(async (request, response) => {
+	const order = await Order.findById(request.params.id);
+
+	if (order) {
+		order.isPaid = true;
+		order.paidAt = Date.now();
+		order.paymentResult = {
+			id: request.body.id,
+			status: request.body.status,
+			update_time: request.body.update_time,
+			email_address: request.body.payer.email_address,
+		};
+
+		const updatedOrder = await order.save();
+
+		response.json(updatedOrder);
+	} else {
+		response.status(404);
+		throw new Error('Order not found');
+	}
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
