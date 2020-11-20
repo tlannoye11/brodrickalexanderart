ae0ec0f1bd470b1df1bebda8724181cd618590b8
Travis <travis.lannoye@gmail.com>
2020-11-20 16:38:01 -0600

    Added routes to get user by id, update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -108,6 +108,38 @@ const deleteUser = asyncHandler(async (request, response) => {
 	}
 });
 
+const getUserById = asyncHandler(async (request, response) => {
+	const user = await User.findById(request.params.id).select('-password');
+	if (user) {
+		response.json(user);
+	} else {
+		response.status(404);
+		throw new Error('User not found');
+	}
+});
+
+const updateUser = asyncHandler(async (request, response) => {
+	const user = await User.findById(request.params.id);
+
+	if (user) {
+		user.name = request.body.name || user.name;
+		user.email = request.body.email || user.email;
+		user.isAdmin = request.body.isAdmin;
+
+		const updatedUser = await user.save();
+
+		response.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+		});
+	} else {
+		response.status(404);
+		throw new Error('User not found');
+	}
+});
+
 export {
 	authUser,
 	registerUser,
@@ -115,4 +147,6 @@ export {
 	updateUserProfile,
 	getUsers,
 	deleteUser,
+	getUserById,
+	updateUser,
 };
