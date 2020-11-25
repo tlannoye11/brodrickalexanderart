3865b3541ee88f0bfb57d8c421ef4def3ee08fcb
Travis <travis.lannoye@gmail.com>
2020-11-24 22:23:13 -0600

    Added product create/update routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,4 +30,57 @@ const deleteProduct = asyncHandler(async (request, response) => {
 	}
 });
 
-export { getProducts, getProductById, deleteProduct };
+const createProduct = asyncHandler(async (request, response) => {
+	const product = new Product({
+		name: 'Sample name',
+		price: 0,
+		user: request.user._id,
+		image: '/images/sample.jpg',
+		brand: 'Sample brand',
+		category: 'Sample category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'Sample description',
+	});
+
+	const createdProduct = await product.save();
+	response.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (request, response) => {
+	const {
+		name,
+		price,
+		description,
+		image,
+		brand,
+		category,
+		countInStock,
+	} = request.body;
+
+	const product = await Product.findById(request.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.description = description;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+
+		const updatedProduct = await product.save();
+		response.json(product);
+	} else {
+		response.status(404);
+		throw new Error('Product not found');
+	}
+});
+
+export {
+	getProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+};
