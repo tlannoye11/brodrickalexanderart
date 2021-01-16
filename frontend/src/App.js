08eaa8d8e941511e7d3d028b7722f6f9cb793e0a
Travis <travis.lannoye@gmail.com>
2021-01-15 22:45:20 -0600

    Moved CSS, added hero image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,6 +23,14 @@ const App = () => {
 	return (
 		<Router>
 			<Header />
+			<div className='hero-image'>
+				<div className='hero-text'>
+					<h1 className='hero-title'>Life Through Art</h1>
+					<h2 className='hero-subtitle'>
+						Original and Commissioned Oil/Digital Art
+					</h2>
+				</div>
+			</div>
 			<main className='py-3'>
 				<Container>
 					<Route path='/order/:id' component={OrderScreen} />
